0be776b56976a02bba5ee61073ba2abeec07104a
Elephes Sung <147816340+ElephesSung@users.noreply.github.com>
2025-08-17 18:57:37 +0100

    Update feed.py

--- feed.py
+++ feed.py
@@ -1,7 +1,7 @@
 import yaml
 import xml.etree.ElementTree as xml_tree
 
-with open('/Users/marsian/Downloads/test-main/feed.yaml', 'r') as file:
+with open('./feed.yaml', 'r') as file:
     yaml_data = yaml.safe_load(file)
 
     rss_element = xml_tree.Element('rss', {
